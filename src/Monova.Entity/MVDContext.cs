fc827a7332305ec9759602c4bd9d73124691b484
Ahmet <akarsuahmet34@outlook.com>
2020-06-14 10:11:58 +0300

    re-created migrations for identity tables

--- src/Monova.Entity/MVDContext.cs
+++ src/Monova.Entity/MVDContext.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Monova.Entity
 {
-    public class MVDContext : IdentityDbContext<MVDUser>
+    public class MVDContext : IdentityDbContext<MVDUser, IdentityRole<Guid>, Guid> // Contextimizi IdentityDbContext den türettik. IdentityUser yani bizim MVDUser daki Id alanı microsoft default olarak text tutuyor. Biz MVDUser daki Id alanını Guid olarak yaptığımızdan dolayı (public class MVDUser : IdentityUser<Guid>) Onunla ilişkili olan IdentityRole Tablosunudaki ilişkiyide Guid olarak yaptık(IdentityDbContext<MVDUser, IdentityRole<Guid>, Guid>)
     {
         public MVDContext(DbContextOptions<MVDContext> options) : base(options)
         {
