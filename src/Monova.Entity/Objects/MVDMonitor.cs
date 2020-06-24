fde661b1ba53da03fe8cecb544938c14b034a7b2
Ahmet <akarsuahmet34@outlook.com>
2020-06-24 07:29:31 +0300

    added migrations that in userId for monitoring model

--- src/Monova.Entity/Objects/MVDMonitor.cs
+++ src/Monova.Entity/Objects/MVDMonitor.cs
@@ -9,6 +9,7 @@ namespace Monova.Entity
     {
         [Key]
         public Guid Id { get; set; }
+        public Guid UserId { get; set; }
         public string Name { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime UpdateTime { get; set; }
