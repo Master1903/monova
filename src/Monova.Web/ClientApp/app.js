b65023013265b7368ed000651a8aeb2e7bc83e84
Ahmet <akarsuahmet34@outlook.com>
2020-06-24 08:51:03 +0300

    added vue notify package

--- src/Monova.Web/ClientApp/app.js
+++ src/Monova.Web/ClientApp/app.js
@@ -5,9 +5,11 @@ import store from './store'
 import { sync } from 'vuex-router-sync'
 import App from 'components/root/app-root'
 import { FontAwesomeIcon } from './icons'
+import Notifications from 'vue-notification'
 
 // Registration of global components
 Vue.component('icon', FontAwesomeIcon)
+Vue.use(Notifications)
 
 Vue.prototype.$http = axios
 
