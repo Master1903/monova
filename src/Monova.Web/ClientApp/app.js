fd19f33e7ec0caff122d2bb93f63c7fae0ced395
Ahmet <akarsuahmet34@outlook.com>
2020-06-28 08:16:02 +0300

    added vue loader placeholder for nice display

--- src/Monova.Web/ClientApp/app.js
+++ src/Monova.Web/ClientApp/app.js
@@ -6,11 +6,14 @@ import { sync } from 'vuex-router-sync'
 import App from 'components/root/app-root'
 import { FontAwesomeIcon } from './icons'
 import Notifications from 'vue-notification'
+import MVIText from '@/components/input/text'
+import VueContentPlaceholders from 'vue-content-placeholders'
 
 // Registration of global components
 Vue.component('icon', FontAwesomeIcon)
+Vue.component('mvi-text', MVIText)
 Vue.use(Notifications)
-
+Vue.use(VueContentPlaceholders)
 Vue.prototype.$http = axios
 
 sync(store, router)
