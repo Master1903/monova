5b8e8002f899b75d5706d012de914958055a0a35
Ahmet <akarsuahmet34@outlook.com>
2020-06-28 08:21:11 +0300

    added base input text component for UI

---
+++ src/Monova.Web/ClientApp/components/input/text.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="form-group">
+    <label for="exampleInputEmail1">{{title}}</label>
+    <input
+      type="text"
+      class="form-control"
+      :placeholder="placeholder"
+      v-on:input="$emit('input', $event.target.value)"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    placeholder: {
+      default: "",
+      type: String
+    },
+    title: {
+      default: "",
+      type: String
+    }
+  }
+};
+</script>
+
+<style>
+</style>
