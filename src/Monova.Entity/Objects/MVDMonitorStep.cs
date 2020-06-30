24dd561c961b8a0187f62000625cdd096355daf5
Ahmet <akarsuahmet34@outlook.com>
2020-06-30 15:08:47 +0300

    added SettingsAsRequest() method for JsCo.DesObjct

--- src/Monova.Entity/Objects/MVDMonitorStep.cs
+++ src/Monova.Entity/Objects/MVDMonitorStep.cs
@@ -1,6 +1,7 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Newtonsoft.Json;
 
 namespace Monova.Entity
 {
@@ -14,6 +15,12 @@ namespace Monova.Entity
 
         //Verileri JSON olarak tuttuğumuz alan
         public string Settings { get; set; }
+
+        public MVDSMonitorStepSettingsRequest SettingsAsRequest()
+        {
+            return JsonConvert.DeserializeObject<MVDSMonitorStepSettingsRequest>(Settings);
+
+        }
 
 
     }
