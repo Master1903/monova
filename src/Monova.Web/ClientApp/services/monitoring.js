b476a68d4ef970fbf9e5fc52dfa3d70bcf5fe516
Ahmet <akarsuahmet34@outlook.com>
2020-06-30 15:12:05 +0300

    added monitoring put method

--- src/Monova.Web/ClientApp/services/monitoring.js
+++ src/Monova.Web/ClientApp/services/monitoring.js
@@ -2,25 +2,28 @@ import { http } from '@/utils/http'
 const MonitoringService = {
   async list () {
     var result = await http.get('/api/v1/monitoring')
-    if (result.status === 200) { return result.data } else {
-    }
+    if (result.status === 200) return result.data
   },
   async getById (id) {
-    if (id) {
-      var result = await http.get(`/api/v1/monitoring/${id}`)
-      if (result.status === 200) {
-        return result.data
-      }
-    }
+    if (!id) return
+    var result = await http.get(`/api/v1/monitoring/${id}`)
+    if (result.status !== 200) return
+
+    return result.data
   },
   async save (value) {
     var result = await http.post('/api/v1/monitoring', value)
 
-    if (result.status === 200) {
-      return result.data
-    } else {
-      throw result.error
-    }
+    if (result.status !== 200) throw result.error
+
+    return result.data
+  },
+  async update (value) {
+    var result = await http.put('/api/v1/monitoring', value)
+
+    if (result.status !== 200) throw result.error
+
+    return result.data
   }
 }
 export default MonitoringService // dışarıdan kullabilmek için export ediyoruz
