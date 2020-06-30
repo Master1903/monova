839c42b931994e93bbef3c8e3a9755292072d940
Ahmet <akarsuahmet34@outlook.com>
2020-06-30 15:10:03 +0300

    added update methods

--- src/Monova.Web/ClientApp/components/pages/monitoring/save.vue
+++ src/Monova.Web/ClientApp/components/pages/monitoring/save.vue
@@ -1,22 +1,35 @@
 <template>
-  <div class="row">
-    <div class="col-md-4">
-      <mvi-text
-        placeholder="Name"
-        title="Project Name"
-        v-model="modelData.name"
-      />
+  <div>
+    <div v-if="loading">
+      <content-placeholders>
+        <content-placeholders-text :lines="3" />
+      </content-placeholders>
     </div>
-    <div class="col-md-4">
-      <mvi-text
-        placeholder="url"
-        title="Project Url"
-        v-model="modelData.url"
-      />
-    </div>
+    <div
+      v-else
+      class="row"
+    >
+      <div class="col-md-4">
+        <mvi-text
+          placeholder="Name"
+          title="Project Name"
+          v-model="modelData.name"
+        />
+      </div>
+      <div class="col-md-4">
+        <mvi-text
+          placeholder="url"
+          title="Project Url"
+          v-model="modelData.url"
+        />
+      </div>
 
-    <div class="col-md-3 mt-4">
-      <button class="btn btn-success" @click="save">Save</button>
+      <div class="col-md-3 mt-4">
+        <button
+          class="btn btn-success"
+          @click="save"
+        >Save</button>
+      </div>
     </div>
   </div>
 </template>
@@ -26,19 +39,37 @@ import service from "services/monitoring";
 export default {
   data() {
     return {
+      loading: true,
       modelData: {
-        name: "",
-        url: ""
+        name: null,
+        url: null
       }
     };
   },
+  async mounted() {
+    if (this.$route.params.id) {
+      const result = await service.getById(this.$route.params.id);
+
+      if (result.data) this.modelData = result.data;
+    }
+    this.loading = false;
+  },
   methods: {
     async save() {
-      var result = await service.save(this.modelData);
-      if (result.success && result.data && result.data.id) {
-        this.$router.push({
-          name: "monitoring-list"
-        });
+      if (this.$route.params.id) {
+        var result = await service.update(this.modelData);
+        if (result.success && result.data && result.data.id) {
+          this.$router.push({
+            name: "monitoring-list"
+          });
+        }
+      } else {
+        var result = await service.save(this.modelData);
+        if (result.success && result.data && result.data.id) {
+          this.$router.push({
+            name: "monitoring-list"
+          });
+        }
       }
     }
   }
